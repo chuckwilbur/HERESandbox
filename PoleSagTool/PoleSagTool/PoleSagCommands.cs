575be964fcf02965ef94fa1e12e082f86280d6ce
Chuck Wilbur <cwilbur01@earthlink.net>
2014-02-21 13:31:32 -0800

    Added skeleton project for pole sag tool

---
+++ PoleSagTool/PoleSagTool/PoleSagCommands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.Runtime;
+using AcadApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace PoleSagTool
+{
+    public class PoleSagCommands
+    {
+        static Autodesk.AutoCAD.EditorInput.Editor Ed
+        {
+            get { return AcadApp.DocumentManager.MdiActiveDocument.Editor; }
+        }
+
+        [CommandMethod("CP")]
+        public void CreatePole()
+        {
+            Ed.WriteMessage("Called Create Pole");
+        }
+
+        [CommandMethod("CS")]
+        public void CreateSpan()
+        {
+            Ed.WriteMessage("Called Create Span");
+        }
+    }
+}
